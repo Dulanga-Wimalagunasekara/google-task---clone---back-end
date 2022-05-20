7c2223f9d144479a8943f7af34c424afe6d8296d
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-05-20 13:03:44 +0530

    Fix an error in TaskServlet
    Remove unnecessary service method in TaskServlet

--- src/main/java/lk/ijse/dep8/tasks/api/TaskListServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/TaskListServlet.java
@@ -11,26 +11,12 @@ import java.io.IOException;
 @WebServlet(name = "TaskListServlet")
 public class TaskListServlet extends HttpServlet2 {
     @Override
-    public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        String pathInfo = req.getPathInfo();
-        String pattern = "/users/[A-Fa-f0-9\\-]{36}/list/?.*";
-        if (pathInfo==null){
-            res.sendError(HttpServletResponse.SC_NOT_FOUND);
-        }
-        if (pathInfo.matches(pattern)){
-            super.service(req,res);
-        }else {
-            res.sendError(HttpServletResponse.SC_NOT_FOUND);
-        }
-    }
-
-    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (req.getContentType() == null || !req.getContentType().startsWith("application/json")) {
             throw new ResponseStatusException(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Invalid Content Type Or Content Type is Empty");
         }
 
-        String pattern = "/users/[A-Fa-f0-9\\-]{36}/list/?.*";
+        String pattern = "/[A-Fa-f0-9\\-]{36}/list/?.*";
         if (!req.getPathInfo().matches(pattern)){
             throw new ResponseStatusException(HttpServletResponse.SC_METHOD_NOT_ALLOWED,"Invalid end point");
         }
