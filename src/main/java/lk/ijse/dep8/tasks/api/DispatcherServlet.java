c971e24f5db8b6ab8146c76b20fad0e9e3619244
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-05-20 12:42:22 +0530

    Remove unnecessary code lines in DispatcherServlet

--- src/main/java/lk/ijse/dep8/tasks/api/DispatcherServlet.java
+++ src/main/java/lk/ijse/dep8/tasks/api/DispatcherServlet.java
@@ -12,14 +12,11 @@ public class DispatcherServlet extends HttpServlet2 {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         if (req.getPathInfo() == null || req.getPathInfo().equals("/")){
-            System.out.println("user servlet");
             getServletContext().getNamedDispatcher("UserServlet").forward(req, resp);
         }else{
             if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/?")){
-                System.out.println("user servlet");
                 getServletContext().getNamedDispatcher("UserServlet").forward(req, resp);
             }else if (req.getPathInfo().matches("/[A-Fa-f0-9\\-]{36}/lists(/\\d+)?/?")){
-                System.out.println("Task servlet");
                 getServletContext().getNamedDispatcher("TaskListServlet").forward(req, resp);
             }
         }
