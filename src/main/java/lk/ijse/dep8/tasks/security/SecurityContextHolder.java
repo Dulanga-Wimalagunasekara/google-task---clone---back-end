e30ebeb3158e68a48ef1c6f170a07657ab193c65
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-05-23 13:21:39 +0530

    Add ThreadLocal principal to SecurityContextHolder

--- src/main/java/lk/ijse/dep8/tasks/security/SecurityContextHolder.java
+++ src/main/java/lk/ijse/dep8/tasks/security/SecurityContextHolder.java
@@ -1,5 +1,16 @@
 package lk.ijse.dep8.tasks.security;
 
+import lk.ijse.dep8.tasks.dto.UserDTO;
+
 public class SecurityContextHolder{
+
+    private static volatile ThreadLocal<UserDTO> principal=new ThreadLocal<>();
+    public static void setPrincipal(UserDTO dto){
+        principal.set(dto);
+    }
+    public static UserDTO getPrincipal(){
+        return principal.get();
+    }
+
 
 }
