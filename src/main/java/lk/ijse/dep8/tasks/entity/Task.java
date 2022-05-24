1f88bc06f4bfe3c8565b21bb4fe842bcbfc286c0
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-05-24 20:56:44 +0530

    Add Lombok to the Project

--- src/main/java/lk/ijse/dep8/tasks/entity/Task.java
+++ src/main/java/lk/ijse/dep8/tasks/entity/Task.java
@@ -1,7 +1,14 @@
 package lk.ijse.dep8.tasks.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Task implements Serializable {
     private int id;
     private String title;
@@ -10,79 +17,7 @@ public class Task implements Serializable {
     private Status status;
     private int taskListId;
 
-    public Task(int id, String title, String details, int position, Status status, int taskListId) {
-        this.id = id;
-        this.title = title;
-        this.details = details;
-        this.position = position;
-        this.status = status;
-        this.taskListId = taskListId;
-    }
-
-    public Task() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDetails() {
-        return details;
-    }
-
-    public void setDetails(String details) {
-        this.details = details;
-    }
-
-    public int getPosition() {
-        return position;
-    }
-
-    public void setPosition(int position) {
-        this.position = position;
-    }
-
-    public Status getStatus() {
-        return status;
-    }
-
-    public void setStatus(Status status) {
-        this.status = status;
-    }
-
-    public int getTaskListId() {
-        return taskListId;
-    }
-
-    public void setTaskListId(int taskListId) {
-        this.taskListId = taskListId;
-    }
-
     public enum Status{
         completed,needsAction
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", details='" + details + '\'' +
-                ", position=" + position +
-                ", status=" + status +
-                ", taskListId=" + taskListId +
-                '}';
     }
 }
--- src/main/java/lk/ijse/dep8/tasks/entity/TaskList.java
+++ src/main/java/lk/ijse/dep8/tasks/entity/TaskList.java
@@ -1,51 +1,17 @@
 package lk.ijse.dep8.tasks.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class TaskList implements Serializable {
     private int id;
     private String name;
     private String userId;
 
-    public TaskList(int id, String name, String userId) {
-        this.id = id;
-        this.name = name;
-        this.userId = userId;
-    }
-
-    public TaskList() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    @Override
-    public String toString() {
-        return "TaskList{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", userId='" + userId + '\'' +
-                '}';
-    }
 }
