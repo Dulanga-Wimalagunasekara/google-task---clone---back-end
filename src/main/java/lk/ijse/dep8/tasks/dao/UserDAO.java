ef1cfccda021eb582ccdef3ec4d00126e020401f
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-05-24 12:56:06 +0530

    Define basic structure of the UserService and UserDAO

--- src/main/java/lk/ijse/dep8/tasks/dao/UserDAO.java
+++ src/main/java/lk/ijse/dep8/tasks/dao/UserDAO.java
@@ -1,5 +1,21 @@
 package lk.ijse.dep8.tasks.dao;
 
+import lk.ijse.dep8.tasks.dto.UserDTO;
+
 public class UserDAO {
+    public static void saveUser(UserDTO user){
+
+    }
+
+    public static void updateUser(UserDTO user){
+
+    }
+
+    public static void deleteUser(String id){
+
+    }
+    public static UserDTO getUser(String id){
+        return null;
+    }
 
 }
