cbcffef324f1789b3824312562fad76fd0d02e36
Dulanga Wimalagunasekara <dulangawimalagunasekara@gmail.com>
2022-05-25 18:36:45 +0530

    Implement the factory design pattern to the dao Layer

--- src/main/java/lk/ijse/dep8/tasks/dao/impl/QueryDAOimpl.java
+++ src/main/java/lk/ijse/dep8/tasks/dao/impl/QueryDAOimpl.java
@@ -1,5 +1,12 @@
 package lk.ijse.dep8.tasks.dao.impl;
 
+import java.sql.Connection;
+
 public class QueryDAOimpl implements QueryDAO{
 
+    private Connection connection;
+
+    public QueryDAOimpl(Connection connection) {
+        this.connection = connection;
+    }
 }
